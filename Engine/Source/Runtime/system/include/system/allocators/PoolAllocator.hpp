4c385cc49cd672aa91e1e1139cf8cf45058f639f
Nic <nicolacognata@gmail.com>
2020-09-23 02:36:07 -0400

    Write allocate and free functions for Screwjank::PoolAllocator

--- Engine/Source/Runtime/system/include/system/allocators/PoolAllocator.hpp
+++ Engine/Source/Runtime/system/include/system/allocators/PoolAllocator.hpp
@@ -4,26 +4,134 @@
 // Library Headers
 
 // Engine Headers
+#include "core/Assert.hpp"
+#include "core/MemorySystem.hpp"
 #include "system/Allocator.hpp"
+#include "system/Memory.hpp"
 
 namespace Screwjank {
 
-    template <size_t block_size>
+    /**
+     * Allocates memory in fixed size chunks
+     */
+    template <size_t t_BlockSize>
     class PoolAllocator : public Allocator
     {
       public:
-        PoolAllocator(size_t buffer_size);
+        /**
+         * Constructor
+         * @param block_count The number of blocks of size t_BlockSize
+         */
+        PoolAllocator(size_t block_count,
+                      Allocator* backing_allocator = MemorySystem::GetDefaultAllocator());
+
+        /**
+         * Allocates size bites from the heap
+         * @param size The number of bytes to allocate
+         */
+        [[nodiscard]] virtual void* Allocate(const size_t size,
+                                             const size_t alignment = 1) override;
+
+        /**
+         * Marks memory as free
+         * @param memory Pointer to the memory to free
+         */
+        virtual void Free(void* memory = nullptr) override;
+
+      private:
+        struct FreeBlock
+        {
+            FreeBlock* Next;
+        };
+
+        Allocator* m_BackingAllocator;
+
+        /** The beginning of this allocator's data buffer */
+        void* m_BufferStart;
+
+        /** Pointer head of singly linked list of free blocks */
+        FreeBlock* m_FreeList;
+
+        /** Number of blocks managed by this allocator */
+        size_t m_NumBlocks;
     };
 
-    template <class T>
-    class ObjectPoolAllocator : public PoolAllocator<sizeof(T)>
+    template <size_t t_BlockSize>
+    inline PoolAllocator<t_BlockSize>::PoolAllocator(size_t num_blocks,
+                                                     Allocator* backing_allocator)
+        : m_BackingAllocator(backing_allocator), m_NumBlocks(num_blocks)
     {
-    };
+        // Ensure block size is large enough to store an allocation header
+        static_assert(t_BlockSize > sizeof(FreeBlock),
+                      "Block size is not large enough to maintain free list");
 
-    template <size_t block_size>
-    inline PoolAllocator<block_size>::PoolAllocator(size_t buffer_size)
+        SJ_ASSERT(num_blocks > 0, "");
+
+        m_BufferStart =
+            m_BackingAllocator->Allocate(num_blocks * t_BlockSize, alignof(std::max_align_t));
+
+        uintptr_t block_address = (uintptr_t)m_BufferStart;
+        m_FreeList = new ((void*)block_address) FreeBlock {nullptr};
+        FreeBlock* curr_block = m_FreeList;
+
+        // Build free list
+        for (size_t i = 1; i < num_blocks; i++) {
+            // Calculate the block address of the next block
+            block_address += (i * t_BlockSize);
+
+            // Create new free-list block
+            curr_block->Next = new ((void*)block_address) FreeBlock();
+
+            // Move the curr block forward
+            curr_block = curr_block->Next;
+        }
+    }
+
+    template <size_t t_BlockSize>
+    inline void* PoolAllocator<t_BlockSize>::Allocate(const size_t size, const size_t alignment)
     {
-        static_assert(buffer_size > block_size);
+        SJ_ASSERT(size <= t_BlockSize,
+                  "Pool allocator cannot satisfy allocation of size > block size");
+
+        if (m_FreeList == nullptr) {
+            SJ_ENGINE_LOG_ERROR("Pool allocator {} has run out of blocks", m_DebugName);
+            return nullptr;
+        }
+
+        // Pull the first block off the free list, and update and remove it from the free list
+        void* free_block = m_FreeList;
+        m_FreeList = m_FreeList->Next;
+
+        // Get required adjustment to align allocation in block
+        auto adjustment = GetAlignmentAdjustment(alignment, free_block);
+
+        if (size + adjustment > t_BlockSize) {
+            SJ_ENGINE_LOG_ERROR("Pool allocator {} cannot satisfy alignment requirement",
+                                m_DebugName);
+            return nullptr;
+        }
+
+        return (void*)((uintptr_t)free_block + adjustment);
+    }
+
+    template <size_t t_BlockSize>
+    inline void PoolAllocator<t_BlockSize>::Free(void* memory)
+    {
+        // Ensure memory is in the region managed by this allocator
+        SJ_ASSERT((uintptr_t)memory >= (uintptr_t)m_BufferStart,
+                  "Memory is not managed by this allocator");
+        SJ_ASSERT((uintptr_t)memory <= (uintptr_t)m_BufferStart + (t_BlockSize * m_NumBlocks),
+                  "Memory is not managed by this allocator");
+
+        // Find out how far memory is from the start of a block
+        auto offset = GetAlignmentOffset(t_BlockSize, memory);
+
+        // Create new free block
+        auto new_block = new ((void*)((uintptr_t)memory - offset)) FreeBlock {nullptr};
+
+        // Add new free block to head of list
+        new_block->Next = m_FreeList;
+        m_FreeList = new_block;
     }
 
 } // namespace Screwjank
---
+++ Engine/Source/Tests/system/PoolAllocator_test.cpp
@@ -0,0 +1,82 @@
+// STD Headers
+
+// Library Headers
+#include "gtest/gtest.h"
+
+// Void Engine Headers
+#include "platform/PlatformDetection.hpp"
+#include "system/Memory.hpp"
+#include "system/allocators/PoolAllocator.hpp"
+
+using namespace Screwjank;
+
+namespace system_tests {
+
+    struct FreeListAllocatorDummy
+    {
+        char Label;
+        double Value;
+    };
+
+    TEST(PoolAllocatorTests, BasicAllocationTest)
+    {
+        PoolAllocator<sizeof(FreeListAllocatorDummy)> allocator(
+            4,
+            MemorySystem::GetDefaultUnmanagedAllocator());
+
+        auto mem_loc1 = allocator.AllocateType<FreeListAllocatorDummy>();
+        ASSERT_NE(nullptr, mem_loc1);
+        auto dummy1 = new (mem_loc1) FreeListAllocatorDummy {'a', 3.14};
+
+        auto mem_loc2 = allocator.AllocateType<FreeListAllocatorDummy>();
+        ASSERT_NE(nullptr, mem_loc2);
+        auto dummy2 = new (mem_loc2) FreeListAllocatorDummy {'b', 3.14};
+
+        auto mem_loc3 = allocator.AllocateType<FreeListAllocatorDummy>();
+        ASSERT_NE(nullptr, mem_loc3);
+        auto dummy3 = new (mem_loc3) FreeListAllocatorDummy {'c', 3.14};
+
+        auto mem_loc4 = allocator.AllocateType<FreeListAllocatorDummy>();
+        ASSERT_NE(nullptr, mem_loc4);
+        auto dummy4 = new (mem_loc4) FreeListAllocatorDummy {'d', 3.14};
+
+        // Should not be able to allocate a 5th block
+        ASSERT_EQ(nullptr, allocator.AllocateType<FreeListAllocatorDummy>());
+
+        // Make sure no memory was corrupted
+        ASSERT_EQ('a', dummy1->Label);
+        ASSERT_EQ('b', dummy2->Label);
+        ASSERT_EQ('c', dummy3->Label);
+        ASSERT_EQ('d', dummy4->Label); // WHAT THE FUCK IS WRONG WITH THIS
+        ASSERT_EQ(3.14, dummy1->Value);
+        ASSERT_EQ(dummy1->Value, dummy2->Value);
+        ASSERT_EQ(dummy2->Value, dummy3->Value);
+        ASSERT_EQ(dummy3->Value, dummy4->Value);
+
+        allocator.Delete<FreeListAllocatorDummy>(dummy3);
+
+        mem_loc3 = allocator.AllocateType<double>();
+        ASSERT_NE(nullptr, mem_loc3);
+
+        auto d = new (mem_loc3) double {1234.56789};
+
+        // Make sure no memory was stomped
+        ASSERT_EQ('b', dummy2->Label);
+        ASSERT_EQ(3.14, dummy2->Value);
+        ASSERT_EQ(3.14, dummy4->Value);
+        ASSERT_EQ('d', dummy4->Label);
+        allocator.Delete<double>(d);
+
+        mem_loc3 = allocator.AllocateType<FreeListAllocatorDummy>();
+        ASSERT_NE(nullptr, mem_loc3);
+        dummy3 = new (mem_loc3) FreeListAllocatorDummy {'c', 3.14};
+
+        ASSERT_EQ('b', dummy2->Label);
+        ASSERT_EQ('c', dummy3->Label);
+        ASSERT_EQ('d', dummy4->Label);
+        ASSERT_EQ(3.14, dummy2->Value);
+        ASSERT_EQ(dummy2->Value, dummy3->Value);
+        ASSERT_EQ(dummy3->Value, dummy4->Value);
+    }
+
+} // namespace system_tests
