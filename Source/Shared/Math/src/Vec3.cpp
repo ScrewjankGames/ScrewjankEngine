4d3cc2510cf194809dcba1a2704140bff9a3e382
Nicolas <nicolacognata@gmail.com>
2023-10-21 22:57:18 -0700

    Add math to shared

--- Source/Shared/Math/src/Vec3.cpp
+++ Source/Shared/Math/src/Vec3.cpp
@@ -1,5 +1,4 @@
 #include <ScrewjankShared/Math/Vec3.hpp>
-#include <ScrewjankShared/Core/Assert.hpp>
 
 sj::Vec3::Vec3(float x, float y, float z) : m_elements {x, y, z}
 {
@@ -8,6 +7,5 @@ sj::Vec3::Vec3(float x, float y, float z) : m_elements {x, y, z}
 
 float& sj::Vec3::operator[](int idx)
 {
-    SJ_ASSERT()
-    // TODO: insert return statement here
+    return m_elements[idx];
 }
