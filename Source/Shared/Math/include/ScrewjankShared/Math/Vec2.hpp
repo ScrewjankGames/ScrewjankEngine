eb6a0c6747180abc9201aba7d8ac8220347a9ca6
Nicolas <nicolacognata@gmail.com>
2023-10-21 22:57:18 -0700

    Add math to shared

--- Source/Shared/Math/include/ScrewjankShared/Math/Vec2.hpp
+++ Source/Shared/Math/include/ScrewjankShared/Math/Vec2.hpp
@@ -8,6 +8,8 @@ namespace sj
         Vec2() = default;
         Vec2(float x, float y);
 
+        float& operator[](int idx);
+
     private:
         float m_elements[2];
 	};
--- Source/Shared/Math/src/Vec2.cpp
+++ Source/Shared/Math/src/Vec2.cpp
@@ -4,3 +4,8 @@ sj::Vec2::Vec2(float x, float y) : m_elements {x, y}
 {
 
 }
+
+float& sj::Vec2::operator[](int idx)
+{
+    return m_elements[idx];
+}
