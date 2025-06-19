8067b68a165971304f59548475533d02ce336949
Nic LaCognata <Nic LaCognata>
2025-06-19 16:57:04 -0500

    Add vcpkg to gcc toolchain

--- CMake/gcc_toolchain.cmake
+++ CMake/gcc_toolchain.cmake
@@ -5,3 +5,5 @@ set(CMAKE_CXX_EXTENSIONS OFF)
 
 set(CMAKE_C_COMPILER /usr/bin/gcc CACHE PATH "C Compiler") # Or the path to your clang
 set(CMAKE_CXX_COMPILER /usr/bin/g++ CACHE PATH "C++ Compiler") # Or the path to your clang++
+
+include($ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake) # Use VCPKG on windows to discover dependencies
