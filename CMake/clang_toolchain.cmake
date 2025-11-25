63043b157dac429d14c2f0c6f2dd235c4747c793
Nic LaCognata <Nic LaCognata>
2025-11-25 01:17:55 -0600

    Fixup ClangASAN build config

--- CMake/clang_toolchain.cmake
+++ CMake/clang_toolchain.cmake
@@ -1,8 +1,6 @@
-
 set(CMAKE_C_COMPILER clang CACHE PATH "C Compiler") # Or the path to your clang
 set(CMAKE_CXX_COMPILER clang++ CACHE PATH "C++ Compiler") # Or the path to your clang++
 
-set(CMAKE_LINKER_TYPE LLD)
 #set(CMAKE_CXX_CLANG_TIDY "clang-tidy")
 #add_compile_options(-W -Wall -Wextra -Werror)
 #set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libstdc++ -Weverything -Werror -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-c++98-c++11-compat-pedantic")
--- CMake/clang_toolchain_asan.cmake
+++ CMake/clang_toolchain_asan.cmake
@@ -1,9 +1,8 @@
 set(CMAKE_C_COMPILER clang CACHE PATH "C Compiler") # Or the path to your clang
 set(CMAKE_CXX_COMPILER clang++ CACHE PATH "C++ Compiler") # Or the path to your clang++
 
-add_compile_options(-W -Wall -Wextra -Werror)
 add_compile_options(-fsanitize=address,undefined)
 add_link_options(-fsanitize=address,undefined)
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libstdc++")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libstdc++")
+include($ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake) # Use VCPKG on windows to discover dependencies
+
